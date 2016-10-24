7812450c29365a826a34f70ff9312a6d2a6101fa
adrien.lepoutre <external.z26alepo@decathlon.com>
2016-10-24 19:38:16 +0200

    add fences on 404

--- source/javascripts/game.js
+++ source/javascripts/game.js
@@ -2,11 +2,13 @@ jQuery(function($) {
 
   if($('#page404').length == 1) {
 
-    var ratio_speed = 2.5;
+    var ratio_speed = 2;
     var size_sprite_window = Math.ceil($(window).width() / 100) * 100;
     var link_size = 119;
     var max = 1071;
     var current_position = 0;
+    var fence_just_spawned = 0;
+    var speed = ratio_speed * size_sprite_window;
     setInterval(function() {
       if(current_position < max) {
         current_position += link_size;
@@ -17,11 +19,12 @@ jQuery(function($) {
       $('.link-anim').css('background-position', '-'+current_position+'px');
     }, 60);
 
-    backgroundScroll($('.ground'), size_sprite_window, ratio_speed * size_sprite_window); 
+    backgroundScroll($('.ground'), size_sprite_window, speed);
+    animateFences();
 
     $(window).resize(function() {
       size_sprite_window = Math.ceil($(window).width() / 100) * 100;
-      backgroundScroll($('.ground'), size_sprite_window, ratio_speed * size_sprite_window); 
+      backgroundScroll($('.ground'), size_sprite_window, speed); 
     });
 
     window.onkeydown = function(e) {
@@ -32,17 +35,40 @@ jQuery(function($) {
       }
     };
 
-    function backgroundScroll(el, width, speed){        
-        el.animate({'background-position' : '-'+width+'px'}, speed, 'linear', function(){
-            el.css('background-position','0');                
-            backgroundScroll(el, width, speed);
-        });
+    function backgroundScroll(el, width, speed) {        
+      el.animate({'background-position' : '-'+width+'px'}, speed, 'linear', function() {
+        el.css('background-position','0');
+        backgroundScroll(el, width, speed);
+      });
+    }
+  
+    function moveFence(el, width, speed) {     
+      el.animate({'right' : width+'px'}, speed, 'linear', function() {
+        el.remove();
+      });
     }
 
     function jumpLink() {
-      $('.link-anim').animate({ bottom: '250px' }, 200, "easeOutExpo", function() {
-        $('.link-anim').stop().animate({ bottom: '100px' }, 100, "easeOutExpo");
+      $('.link-anim').animate({ bottom: '250px' }, 300, "easeOutExpo", function() {
+        $('.link-anim').stop().animate({ bottom: '100px' }, 200, "easeOutExpo");
       });
+    }
+
+    function animateFences() {
+      setInterval(function() {
+        if(Math.floor((Math.random() * 10) + 1) == 1 && fence_just_spawned == 0) {
+          var $fence = $("<div>", {"class": "fence"});
+          $('.fences').append($fence);
+          moveFence($fence, size_sprite_window, speed + 140);
+          fence_just_spawned = 100;
+        }
+        if(fence_just_spawned > 0) {
+          fence_just_spawned += 100;
+        }
+        if(fence_just_spawned >= 1000) {
+          fence_just_spawned = 0;
+        }
+      }, 100);
     }
   }
 
